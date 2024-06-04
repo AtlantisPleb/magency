09561d340c2feb89e3e60f3c188604d7bbf28a04
Christopher David <chris@arcadelabs.co>
2024-06-04 10:38:54 -0700

    convert all to screens

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -1,26 +1,10 @@
-import { StyleSheet, View } from 'react-native';
+import { Screen } from '@/components/Screen';
+import { View } from 'react-native';
 
 export default function FeedScreen() {
   return (
-    <View style={styles.container}>
-
-    </View>
+    <Screen title="Feed">
+      <View />
+    </Screen>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
