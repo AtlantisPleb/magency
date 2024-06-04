68ec5f83b1bfced54206ddc124c6afa5053667ae
Christopher David <chris@arcadelabs.co>
2024-06-04 11:00:48 -0700

    start global style

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,15 +1,14 @@
 import { Screen } from '@/components/Screen';
 import { View, Text } from 'react-native';
 import { useStore } from '@/lib/store';
+import { styles } from '@/lib/styles';
 
 export default function AccountScreen() {
   const userPubkey = useStore(state => state.userPubkey);
-  console.log('userPubkey', userPubkey)
-
   return (
     <Screen title="Account">
       <View style={{ padding: 30 }}>
-        {userPubkey ? <Text style={{ color: 'white' }}>Your Public Key: {userPubkey}</Text> : <Text style={{ color: 'white' }}>Loading...</Text>}
+        {userPubkey ? <Text style={styles.text}>Your Public Key: {userPubkey}</Text> : <Text style={styles.text}>Loading...</Text>}
       </View>
     </Screen>
   );
