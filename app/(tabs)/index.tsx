4aac137e43190039fc7c25ff21cafc194ed5462d
Christopher David <chris@arcadelabs.co>
2024-06-04 09:42:10 -0500

    cast button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function CastScreen() {
   return (
@@ -8,22 +8,12 @@ export default function CastScreen() {
         numberOfLines={3}
         spellCheck={false}
         autoCorrect={false}
-        style={{
-          backgroundColor: '#0a0a0a',
-          width: '100%',
-          textAlign: "center",
-          borderRadius: 15,
-          borderColor: '#444',
-          borderWidth: 1,
-          color: 'white',
-          fontSize: 18,
-          lineHeight: 23,
-          paddingTop: 18,
-          paddingHorizontal: 24,
-          paddingBottom: 22,
-          fontFamily: 'Courier'
-        }} />
-    </View>
+        style={styles.input}
+      />
+      <TouchableOpacity activeOpacity={0.8} style={styles.submitButton}>
+        <Text style={styles.submitButtonText}>Cast</Text>
+      </TouchableOpacity>
+    </View >
   );
 }
 
@@ -38,9 +28,30 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  input: {
+    backgroundColor: '#0a0a0a',
+    width: '100%',
+    textAlign: "center",
+    borderRadius: 15,
+    borderColor: '#444',
+    borderWidth: 1,
+    color: 'white',
+    fontSize: 18,
+    lineHeight: 23,
+    paddingTop: 18,
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+    fontFamily: 'Courier'
   },
+  submitButton: {
+    marginTop: 25,
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    padding: 24,
+    borderRadius: 4
+  },
+  submitButtonText: {
+    color: 'white',
+    fontSize: 24,
+    fontFamily: 'Courier'
+  }
 });
