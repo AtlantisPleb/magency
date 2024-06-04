0470495625a64e05fad656d7e0b63acd245ddda7
Christopher David <chris@arcadelabs.co>
2024-06-04 05:46:44 -0500

    init ndk, missing TextDecoder

--- lib/useNostr.ts
+++ lib/useNostr.ts
@@ -1,7 +1,14 @@
 import { useEffect } from "react"
+import NDK from "@nostr-dev-kit/ndk";
 
 export const useNostr = () => {
   useEffect(() => {
     console.log('Nostr is the best!')
   }, [])
+
+  // Create a new NDK instance with explicit relays
+  const ndk = new NDK({
+    // explicitRelayUrls: ["wss://a.relay", "wss://another.relay"],
+  });
+  console.log("NDK:", ndk);
 }
